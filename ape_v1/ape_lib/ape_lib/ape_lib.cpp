b6b7cf1a366b554e5203a88d03fd7955ec1887f9
alan king <alanking@alan-mbp.local>
2018-08-03 13:24:09 +0800

    Add spine member ship.

--- ape_v1/ape_lib/ape_lib/ape_lib.cpp
+++ ape_v1/ape_lib/ape_lib/ape_lib.cpp
@@ -11,38 +11,43 @@
 #include "ape_libPriv.hpp"
 #include "ape.hpp"
 
-
-class nsAI::CBrain: public nsAI::CNoCopyable
-{
-public:
-    ~CBrain() final = default;
-};
-
-class nsAI::CInput: public nsAI::CNoCopyable
-{
-public:
-    ~CInput() final = default;
-};
-
-class nsAI::COutput: public nsAI::CNoCopyable
-{
-public:
-    ~COutput() final = default;
-};
-
 class nsAI::CSpine: public nsAI::CNoCopyable
 {
 public:
     ~CSpine() final = default;
 };
 
+class nsAI::CBrain: public nsAI::CNoCopyable
+{
+public:
+    CBrain() = delete;
+    CBrain(std::shared_ptr<CSpine>) {};
+    ~CBrain() final = default;
+};
+
+class nsAI::CInput: public nsAI::CNoCopyable
+{
+public:
+    CInput() = delete;
+    CInput(std::shared_ptr<CSpine>) {};
+    ~CInput() final = default;
+};
+
+class nsAI::COutput: public nsAI::CNoCopyable
+{
+public:
+    COutput() = delete;
+    COutput(std::shared_ptr<CSpine>) {};
+    ~COutput() final = default;
+};
+
 nsAI::CApe::CApe()
 {
 #if 1
     m_spSpine = std::make_shared<CSpine>();
-    m_spBrain = std::make_shared<CBrain>();
-    m_spInput = std::make_shared<CInput>();
-    m_spOutput = std::make_shared<COutput>();
+    m_spBrain = std::make_shared<CBrain>(m_spSpine);
+    m_spInput = std::make_shared<CInput>(m_spSpine);
+    m_spOutput = std::make_shared<COutput>(m_spSpine);
 #else
     m_input.connect(&m_NeurBus);
     m_output.connect(&m_NeurBus);
--- ape_v1/include/ape.hpp
+++ ape_v1/include/ape.hpp
@@ -32,11 +32,13 @@ namespace nsAI
         nsNeuronal::CBusServer m_NeurBus;
 #else
         std::shared_ptr<CBrain> m_spBrain;
+        std::shared_ptr<CSpine> m_spSpine;
         std::shared_ptr<CInput> m_spInput;
         std::shared_ptr<COutput> m_spOutput;
-        std::shared_ptr<CSpine> m_spSpine;
 #endif
-    };}
+    };
+    
+}
 
 
 #pragma GCC visibility pop
