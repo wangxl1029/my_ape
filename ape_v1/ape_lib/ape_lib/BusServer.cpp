f0da53d19c68b52ebbe49c705a4035761c24dce7
alan king <alanking@alan-mbp.local>
2018-08-06 19:04:01 +0800

    Commit the missing files.

--- ape_v1/ape_lib/ape_lib/BusServer.cpp
+++ ape_v1/ape_lib/ape_lib/BusServer.cpp
@@ -6,44 +6,5 @@
 //  Copyright © 2018年 alan king. All rights reserved.
 //
 
-#include <iostream>
-#include "BusServer.hpp"
 
-namespace ns_ = nsAI::nsNeuronal;
 
-void ns_::CBusServer::Send(std::unique_ptr<nsBus::CMessage> m)
-{
-    CActiveTarget* target = nullptr;
-    if ( m->m_ID >= nsBus::CMessageId_E::CORTEX_TEST && m->m_ID < nsBus::CMessageId_E::CORTEX_ERROR )
-    {
-        target = m_pCortex.load();
-    }
-    else if (m->m_ID >= nsBus::CMessageId_E::INPUT_TEST && m->m_ID < nsBus::CMessageId_E::INPUT_ERROR)
-    {
-        target = m_pInput.load();
-    }
-    
-    if (target)
-    {
-        target->PushMessage(std::move(m));
-    }
-}
-
-void ns_::CBusServer::Connect(CConnectiveTarget_E type, CActiveTarget* target)
-{
-    switch (type)
-    {
-        case IBusServer::CConnectiveTarget_E::cortex:
-            m_pCortex.store( target );
-            break;
-        case IBusServer::CConnectiveTarget_E::input:
-            m_pInput.store( target );
-            break;
-        case IBusServer::CConnectiveTarget_E::output:
-            m_pOutput.store(target);
-            break;
-        default:
-            std::cerr << "Unknown target" << std::endl;
-            break;
-    }
-}
--- ape_v1/ape_lib/ape_lib/ape_spine.cpp
+++ ape_v1/ape_lib/ape_lib/ape_spine.cpp
@@ -5,5 +5,48 @@
 //  Created by alan king on 2018/8/3.
 //  Copyright © 2018年 alan king. All rights reserved.
 //
+#include <iostream>
 
 #include "ape.hpp"
+#include "BusMsgTarget.hpp"
+#include "IBusServer.hpp"
+#include "ape_spine.hpp"
+
+namespace ns_ = nsAI::nsNeuronal;
+
+void nsAI::CApe::CSpine::Send(std::unique_ptr<ns_::nsBus::CMessage> m)
+{
+    ns_::nsBus::CMessageTarget_t* target = nullptr;
+    if ( m->m_ID >= ns_::nsBus::CMessageId_E::CORTEX_TEST && m->m_ID < ns_::nsBus::CMessageId_E::CORTEX_ERROR )
+    {
+        target = m_pCortex.load();
+    }
+    else if (m->m_ID >= ns_::nsBus::CMessageId_E::INPUT_TEST && m->m_ID < ns_::nsBus::CMessageId_E::INPUT_ERROR)
+    {
+        target = m_pInput.load();
+    }
+    
+    if (target)
+    {
+        target->PushMessage(std::move(m));
+    }
+}
+
+void nsAI::CApe::CSpine::Connect(ns_::IBusServer::CConnectiveTarget_E type, ns_::nsBus::CMessageTarget_t* target)
+{
+    switch (type)
+    {
+        case ns_::IBusServer::CConnectiveTarget_E::cortex:
+            m_pCortex.store( target );
+            break;
+        case ns_::IBusServer::CConnectiveTarget_E::input:
+            m_pInput.store( target );
+            break;
+        case ns_::IBusServer::CConnectiveTarget_E::output:
+            m_pOutput.store(target);
+            break;
+        default:
+            std::cerr << "Unknown target" << std::endl;
+            break;
+    }
+}
