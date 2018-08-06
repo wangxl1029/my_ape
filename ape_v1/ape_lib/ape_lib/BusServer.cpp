6e7022aea22d178d90405e0343aa12eda3782144
wangxl1029 <wangxl1029@sina.com>
2018-08-06 18:48:23 +0800

    delete the deprecated codes from bus server cpp

--- ape_v1/ape_lib/ape_lib/BusServer.cpp
+++ ape_v1/ape_lib/ape_lib/BusServer.cpp
@@ -7,43 +7,4 @@
 //
 
 #include <iostream>
-#include "BusServer.hpp"
 
-namespace ns_ = nsAI::nsNeuronal;
-
-void ns_::CBusServer::Send(std::unique_ptr<nsBus::CMessage> m)
-{
-    CActiveTarget* target = nullptr;
-    if ( m->m_ID >= nsBus::CMessageId_E::CORTEX_TEST && m->m_ID < nsBus::CMessageId_E::CORTEX_ERROR )
-    {
-        target = m_pCortex.load();
-    }
-    else if (m->m_ID >= nsBus::CMessageId_E::INPUT_TEST && m->m_ID < nsBus::CMessageId_E::INPUT_ERROR)
-    {
-        target = m_pInput.load();
-    }
-    
-    if (target)
-    {
-        target->PushMessage(std::move(m));
-    }
-}
-
-void ns_::CBusServer::Connect(CConnectiveTarget_E type, CActiveTarget* target)
-{
-    switch (type)
-    {
-        case IBusServer::CConnectiveTarget_E::cortex:
-            m_pCortex.store( target );
-            break;
-        case IBusServer::CConnectiveTarget_E::input:
-            m_pInput.store( target );
-            break;
-        case IBusServer::CConnectiveTarget_E::output:
-            m_pOutput.store(target);
-            break;
-        default:
-            std::cerr << "Unknown target" << std::endl;
-            break;
-    }
-}
