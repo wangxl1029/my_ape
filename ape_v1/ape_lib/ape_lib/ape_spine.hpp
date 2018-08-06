bd1fbcfd24ac125089a70c6727121567c1c783da
alan king <alanking@alan-mbp.local>
2018-08-06 18:58:28 +0800

    Commit the missed ape spine header.

---
+++ ape_v1/ape_lib/ape_lib/ape_spine.hpp
@@ -0,0 +1,30 @@
+//
+//  ape_spine.hpp
+//  ape_lib
+//
+//  Created by alan king on 2018/8/3.
+//  Copyright © 2018年 alan king. All rights reserved.
+//
+
+#ifndef ape_spine_hpp
+#define ape_spine_hpp
+
+#include "ape.hpp"
+#include "IBusServer.hpp"
+
+class nsAI::CApe::CSpine: public nsAI::nsNeuronal::IBusServer
+{
+public:
+    CSpine() = default;
+    ~CSpine() final = default;
+    
+    void Send(std::unique_ptr<nsAI::nsNeuronal::nsBus::CMessage> m) final;
+    void Connect(CConnectiveTarget_E type, nsAI::nsNeuronal::nsBus::CMessageTarget_t* target) final;
+
+private:
+    std::atomic< nsAI::nsNeuronal::nsBus::CMessageTarget_t* > m_pInput;
+    std::atomic< nsAI::nsNeuronal::nsBus::CMessageTarget_t* > m_pOutput;
+    std::atomic< nsAI::nsNeuronal::nsBus::CMessageTarget_t* > m_pCortex;
+};
+
+#endif /* ape_spine_hpp */
